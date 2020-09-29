51c3b9e10ad84377bdb0b8fe36f89abadc46b600
harshgoel05 <harshgoel05@gmail.com>
2020-09-29 22:28:48 +0530

    feat(api) : join class api

--- api/class/class-service.ts
+++ api/class/class-service.ts
@@ -1,6 +1,9 @@
-import { Code } from "bson";
-import { getDbClient, initDbClient } from "../utils/database";
-import { UNAUTHORIZED_ERROR } from "../utils/errors";
+import { getDbClient } from "../utils/database";
+import {
+  UNAUTHORIZED_ERROR,
+  CLASSROOM_DOESNOT_EXISTS,
+  ALREADY_JOINED_CLASSROOM,
+} from "../utils/errors";
 import { classSchema } from "./class-model";
 
 export async function createClass(classData: any, user: any) {
@@ -8,7 +11,7 @@ export async function createClass(classData: any, user: any) {
   if (user.role && user.role != "teacher")
     throw { code: 401, message: UNAUTHORIZED_ERROR };
   const dbClient = await getDbClient();
-  let class_code = "";
+  let class_code = ("DK" + Math.floor(Math.random() * 90000) + 10000) as string;
   while (
     await dbClient.db().collection("classrooms").findOne({ code: class_code })
   ) {
@@ -30,4 +33,26 @@ export async function deleteClass(classid: any, user: any) {
   const dbClient = await getDbClient();
 
   await dbClient.db().collection("classrooms").deleteOne({ code: classid });
+}
+
+export async function joinClass(classid: any, user: any) {
+  if (!user || !user.role) throw { code: 401, message: UNAUTHORIZED_ERROR };
+  if (user.role && user.role != "student")
+    throw { code: 401, message: UNAUTHORIZED_ERROR };
+  const dbClient = await getDbClient();
+  let classroom = await dbClient
+    .db()
+    .collection("classrooms")
+    .findOne({ code: classid });
+  if (!classroom) throw { code: 404, message: CLASSROOM_DOESNOT_EXISTS };
+  if (classroom.students.find((student: any) => student == user.email))
+    throw { code: 409, message: ALREADY_JOINED_CLASSROOM };
+  classroom.students.push(user.email);
+  await dbClient
+    .db()
+    .collection("classrooms")
+    .findOneAndUpdate(
+      { code: classid },
+      { $set: { students: classroom.students } }
+    );
 }
